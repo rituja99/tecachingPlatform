9255fb8b861f9d57cea596f8ed709058b02af34f
rituja99 <ritujkalita1999@gmail.com>
2021-07-22 19:33:17 +0530

    modify practice tests student

--- ENpracticeTests.aspx
+++ ENpracticeTests.aspx
@@ -1,38 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="ENpracticeTests.aspx.cs" Inherits="teachingPlatform.ENpracticeTests" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0);
-  transition: 0.3s;
-  width: 40%;
-}
 
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0);
-}
+    <style type="text/css">
+        .auto-style1 {
+            height: 372px;
+            margin-left: 71px;
+        }
+    </style>
 
-.container {
-  padding: 2px 16px;
-}
-    .auto-style1 {
-        box-shadow: 0 4px 8px 0 rgba(0,0,0);
-        transition: 0.3s;
-        width: 42%;
-        height: 41px;
-        margin-left: 39px;
-        margin-top: 31px;
-    }
-</style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div class="auto-style1" style="background-color:powderblue">
-  <div class="container">
-      <asp:Label ID="Label1" Font-Size="Larger" runat="server" Text="1. "/>  
-      <asp:HyperLink ID="HyperLink1" runat="server" Text="Practice Test-1" Font-Size="Larger"
-          ForeColor="#000000" Font-Underline="false"
-          NavigateUrl="https://forms.office.com/Pages/ResponsePage.aspx?id=Qr2-Kf_xPUyWiAZ-NGDcH1GoZ25E7ShBk0qdRvRrjBZUN05aWTk1MzJNTEJOU0hSUjFRUTlTQlpPUS4u"  target="_blank"/>
-      <br />
-  </div>
-</div>
+  <div class="auto-style1">
+      <asp:Panel ID="Panel1" runat="server" Height="371px"/>
+  </div>  
 </asp:Content>
--- ENpracticeTests.aspx.cs
+++ ENpracticeTests.aspx.cs
@@ -4,18 +4,57 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
 
 namespace teachingPlatform
 {
-    public partial class ENpracticeTests : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            Site1 master = (Site1)this.Master;
-            master.StudyMaterials.NavigateUrl = "ENstudyMaterials.aspx";
-            master.PracticeTests.NavigateUrl = "ENpracticeTests.aspx";
-            master.HomePage.NavigateUrl = "StudentHomepage.aspx";
-            master.DiscussionForum.NavigateUrl = "ENdiscussionForum.aspx";
-        }
-    }
+	public partial class ENpracticeTests : System.Web.UI.Page
+	{
+		string connstr = WebConfigurationManager.ConnectionStrings["StudentsDB"].ConnectionString;
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Site1 master = (Site1)this.Master;
+			master.StudyMaterials.NavigateUrl = "ENstudyMaterials.aspx";
+			master.PracticeTests.NavigateUrl = "ENpracticeTests.aspx";
+			master.HomePage.NavigateUrl = "StudentHomepage.aspx";
+			master.DiscussionForum.NavigateUrl = "ENdiscussionForum.aspx";
+
+			Panel1.Controls.Clear();
+			Panel1.Controls.Add(new LiteralControl("<br/>"));
+
+			SqlConnection con = new SqlConnection(connstr);
+			SqlCommand command = new SqlCommand("SELECT Link, TestName FROM PracticeTests WHERE Language='English'", con);
+			SqlDataReader reader;
+			try
+			{
+				con.Open();
+				reader = command.ExecuteReader();
+				while (reader.Read())
+				{
+					Panel newPanel = new Panel();
+
+					HyperLink hyperlink = new HyperLink();
+					hyperlink.NavigateUrl = reader["Link"].ToString();
+					hyperlink.Text = reader["TestName"].ToString();
+					hyperlink.Target = "_blank";
+					//hyperlink.ForeColor = Color.FromArgb(0, 97, 216);
+					hyperlink.ForeColor = Color.Black;
+
+					newPanel.Controls.Add(hyperlink);
+
+					Panel1.Controls.Add(newPanel);
+					Panel1.Controls.Add(new LiteralControl("<br/>"));
+				}
+			}
+			catch(Exception ex)
+			{
+				Label label = new Label();
+				label.Text = ex.Message;
+				Panel1.Controls.Add(label);
+			}
+
+		}
+	}
 }
